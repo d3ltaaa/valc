1c8b9a17af423a82bc32e27d94ac7c452f1c6e1b
d3ltaaa <hil.falk@protonmail.com>
2023-06-08 15:13:35 +0200

    exe

--- iscript/valc-install-part-2.sh
+++ iscript/valc-install-part-2.sh
@@ -1,96 +1,164 @@
 #!/bin/bash
 
+exe () {
+    exit_code=1
+    while [ $exit_code -ne 0 ]; do
+
+        $1
+
+        # check if everything worked
+        if [ $? -eq 0 ]; then
+            exit_code=0
+            echo "Checks out"; sleep 1
+            break
+        fi
+
+        while true; do
+            read -p "Something went wrong here :( Do you want to redo the command? [y/n]: " yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) exit_code=0; break;;
+                * ) echo "Enter 'y' or 'n'!";;
+            esac
+        done
+    done
+}
 
 notification () {
     clear; echo "$1"; sleep 1
 }
 
-# set a new index for parallel downloads
-# first install sed
-notification "Installing sed"
-pacman -S --noconfirm sed
+ena_parallel () {
 
-notification "Enable parallel downloads"
-sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf 
+    # set a new index for parallel downloads
+    # first install sed
+    notification "Installing sed"
+    pacman -S --noconfirm sed
 
-# time
-notification "Time setup"
-ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
-
-hwclock --systohc
-
-# language
-notification "Language setup"
-echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-locale-gen
-echo "LANG=en_US.UTF-8" > /etc/locale.conf
-
-# keyboard layout
-notification "Keyboard layout"
-ans_layout=""
-while true; do
-
-    read -p "Do you want to use the German keyboard layout? [y/n]: " yn
-
-    case $yn in
-
-        [yY]* ) echo "KEYMAP=de-latin1" > /etc/vconsole.conf; break;;
-        [nN]* ) echo "Not accessible yet"; sleep 2;;
-        * ) echo "Enter 'y' or 'n'!";;
-    esac
-done
+    notification "Enable parallel downloads"
+    sed -i "s/^#ParallelDownloads = 5$/ParallelDownloads = 15/" /etc/pacman.conf 
+}
 
 
+time_setup () {
     
+    # time
+    notification "Time setup"
+    ln -sf /usr/share/zoneinfo/Europe/Berlin /etc/localtime
 
+    hwclock --systohc
+}
 
-# Hostname
-notification "Hostname"
-echo "HOSTNAME: "
-read hostname
-echo $hostname > /etc/hostname
+language_setup () {
+    # language
+    notification "Language setup"
+    echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
+    locale-gen
+    echo "LANG=en_US.UTF-8" > /etc/locale.conf
+}
 
-echo "127.0.0.1       localhost" >> /etc/hosts
-echo "::1             localhost" >> /etc/hosts
-echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
+kb_setup () {
+    
+    # keyboard layout
+    notification "Keyboard layout"
+    ans_layout=""
+    while true; do
 
+        read -p "Do you want to use the German keyboard layout? [y/n]: " yn
 
+        case $yn in
 
+            [yY]* ) echo "KEYMAP=de-latin1" > /etc/vconsole.conf; break;;
+            [nN]* ) echo "Not accessible yet"; sleep 2;;
+            * ) echo "Enter 'y' or 'n'!";;
+        esac
+    done
+}
 
-# User
-notification "User"
-passwd
-echo "USER: "
-read user
-useradd -m $user
-echo "Password for $user: "
-passwd $user
-usermod -aG wheel,audio,video $user
-pacman --noconfirm -S sudo
-sudo sed -i '/^# %wheel ALL=(ALL:ALL) ALL$/s/^# //' /etc/sudoers | sudo EDITOR='tee -a' visudo
+host_setup () {
 
+    # Hostname
+    notification "Hostname"
+    echo "HOSTNAME: "
+    read hostname
+    echo $hostname > /etc/hostname
 
-notification "Installing important packages"
-pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+    echo "127.0.0.1       localhost" >> /etc/hosts
+    echo "::1             localhost" >> /etc/hosts
+    echo "127.0.1.1       $hostname.localdomain $hostname" >> /etc/hosts
 
-# grub
-notification "Setting up grub"
-grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader=GRUB --removable
-# sed -i 's/quiet/pci=noaer/g' /etc/default/grub
-# sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
-grub-mkconfig -o /boot/grub/grub.cfg
+}
 
+user_setup () {
 
-# network manager 
-notification "Installing and setting up Networkmanager"
-pacman --noconfirm -S networkmanager
-systemctl enable NetworkManager
+    # User
+    notification "User"
+    passwd
+    echo "USER: "
+    read user
+    useradd -m $user
+    echo "Password for $user: "
+    passwd $user
+    usermod -aG wheel,audio,video $user
+    pacman --noconfirm -S sudo
+    sudo sed -i '/^# %wheel ALL=(ALL:ALL) ALL$/s/^# //' /etc/sudoers | sudo EDITOR='tee -a' visudo
 
+}
 
+inst_important () {
 
-notification "Installing next valc-installation-part"
-curl https://raw.githubusercontent.com/d3ltaaa/valc/main/iscript/valc-install-part-3.sh > valc-install-part-3.sh
-chmod +x valc-install-part-3.sh
+    notification "Installing important packages"
+    pacman -S --noconfirm grub efibootmgr dosfstools os-prober mtools
+
+}
+
+grub_setup () {
+
+    # grub
+    notification "Setting up grub"
+    grub-install --target=x86_64-efi --efi-directory=/boot/EFI --bootloader=GRUB --removable
+    # sed -i 's/quiet/pci=noaer/g' /etc/default/grub
+    # sed -i 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+    grub-mkconfig -o /boot/grub/grub.cfg
+
+}
+
+network_manager () {
+
+    # network manager 
+    notification "Installing and setting up Networkmanager"
+    pacman --noconfirm -S networkmanager
+    systemctl enable NetworkManager
+
+}
+
+inst_part () {
+
+    notification "Installing next valc-installation-part"
+    curl https://raw.githubusercontent.com/d3ltaaa/valc/main/iscript/valc-install-part-3.sh > valc-install-part-3.sh
+    chmod +x valc-install-part-3.sh
+
+}
+
+exe ena_parallel
+
+exe time_setup
+
+exe language_setup
+
+exe kb_setup
+
+exe host_setup 
+
+exe user_setup
+
+exe inst_important
+
+exe grub_setup
+
+exe network_manager
+
+exe inst_part
 
 clear
 echo "The next commands are: "
--- iscript/valc-install-part-3.sh
+++ iscript/valc-install-part-3.sh
@@ -1,66 +1,128 @@
 #!/bin/bash
 
+
+exe () {
+    exit_code=1
+    while [ $exit_code -ne 0 ]; do
+
+        $1
+
+        # check if everything worked
+        if [ $? -eq 0 ]; then
+            exit_code=0
+            echo "Checks out"; sleep 1
+            break
+        fi
+
+        while true; do
+            read -p "Something went wrong here :( Do you want to redo the command? [y/n]: " yn
+            case $yn in
+                [Yy]* ) break;;
+                [Nn]* ) exit_code=0; break;;
+                * ) echo "Enter 'y' or 'n'!";;
+            esac
+        done
+    done
+}
+
 notification () {
     clear; echo "$1"; sleep 1
 }
 
-# update grub 
-notification "Setting up Grub"
-sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+grub_setup () {
 
-# video driver
-notification "Installing video driver"
-while true; do
-    read -p "Does the device have an amd-gpu? [y/n]: " yn
-    case $yn in
-        [yY]* ) sudo pacman --noconfirm -S xf86-video-amdgpu; break;;
-        [nN]* ) sudo pacman --noconfirm -S xf86-video-fbdev; break;;
-        * ) echo "Enter 'y' or 'n'!";;
-    esac
-done
+    # update grub 
+    notification "Setting up Grub"
+    sudo sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=0/' /etc/default/grub
+    sudo grub-mkconfig -o /boot/grub/grub.cfg
 
-notification "Installing packages"
-sudo pacman --noconfirm -S xorg xorg-xinit webkit2gtk base-devel \
-	alsa-utils pulseaudio pavucontrol \
-	bluez bluez-utils pulseaudio-bluetooth blueman \
-	firefox thunar \
-	lf feh xdotool zathura zathura-pdf-mupdf \
-    xournalpp discord \
-	neofetch ranger git neovim dunst xwallpaper xclip acpi upower \
-	flatpak xdg-desktop-portal-gtk unzip \
-	fuse2 ripgrep pamixer sox \
-	imagemagick
+}
 
+video_setup () {
 
-# bluetooth
-notification "Enabling Bluetooth and Audio"
-systemctl enable bluetooth.service
-systemctl --user enable pulseaudio
+    # video driver
+    notification "Installing video driver"
+    while true; do
+        read -p "Does the device have an amd-gpu? [y/n]: " yn
+        case $yn in
+            [yY]* ) sudo pacman --noconfirm -S xf86-video-amdgpu; break;;
+            [nN]* ) sudo pacman --noconfirm -S xf86-video-fbdev; break;;
+            * ) echo "Enter 'y' or 'n'!";;
+        esac
+    done
+}
 
+inst_packages () {
 
-# yay-AUR-helper
-notification "Installing Yay-AUR-helper"
-mkdir ~/.yay
+    notification "Installing packages"
+    sudo pacman --noconfirm -S xorg xorg-xinit webkit2gtk base-devel \
+    	alsa-utils pulseaudio pavucontrol \
+    	bluez bluez-utils pulseaudio-bluetooth blueman \
+    	firefox thunar \
+    	lf feh xdotool zathura zathura-pdf-mupdf \
+        xournalpp discord \
+    	neofetch ranger git neovim dunst xwallpaper xclip acpi upower \
+    	flatpak xdg-desktop-portal-gtk unzip \
+    	fuse2 ripgrep pamixer sox \
+    	imagemagick
 
-cd ~/.yay
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
+}
 
+blue_setup () {
 
-# spotify
-notification "Installing yay packages"
-yay -S ncspot --noconfirm
-yay -S brillo --noconfirm
-yay -S picom-jonaburg-git --noconfirm
+    # bluetooth
+    notification "Enabling Bluetooth and Audio"
+    systemctl enable bluetooth.service
+    systemctl --user enable pulseaudio
 
+}
 
-# remnote
-notification "Installing Remnote"
-curl -L -o remnote https://www.remnote.com/desktop/linux
-chmod +x remnote
-sudo mv remnote /opt
+yay_setup () {
+
+    # yay-AUR-helper
+    notification "Installing Yay-AUR-helper"
+    mkdir ~/.yay
+    
+    cd ~/.yay
+    git clone https://aur.archlinux.org/yay.git
+    cd yay
+    makepkg -si --noconfirm
+
+}
+
+yay_installations () {
+
+    notification "Installing yay packages"
+    yay -S ncspot --noconfirm
+    yay -S brillo --noconfirm
+    yay -S picom-jonaburg-git --noconfirm
+
+}
+
+inst_remnote () {
+
+    # remnote
+    notification "Installing Remnote"
+    curl -L -o remnote https://www.remnote.com/desktop/linux
+    chmod +x remnote
+    sudo mv remnote /opt
+
+}
+
+exe grub_setup
+
+exe video_setup
+
+exe inst_packages
+
+exe blue_setup
+
+exe yay_setup
+
+exe yay_installations
+
+exe inst_remnote
+
 
 
 
