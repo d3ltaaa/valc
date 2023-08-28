679d2fa6853efd022bcb66c4e31e381a5291ff2c
d3ltaaa <you@example.com>
2023-08-28 17:41:02 +0200

    iscript ssh files

--- iscript/valc-install-part-3.sh
+++ iscript/valc-install-part-3.sh
@@ -198,7 +198,9 @@ links_setup () {
     ln -s ~/.valc/setup/.xinitrc ~/ &&
     ln -s ~/.valc/setup/.bash_profile ~/ &&
     ln -s ~/.valc/setup/.bashrc ~/ &&
-    ln -s ~/.valc/setup/.xbindkeysrc ~/ 
+    ln -s ~/.valc/setup/.xbindkeysrc ~/ &&
+    ln -s ~/.valc/setup/ssh_files/config ~/.ssh &&
+    ln -s ~/.valc/setup/ssh_files/repl_ip_in_ssh_config.sh ~/.ssh
     [ $? -ne 0 ] && return 49 || :
 
 }
