2c5cef3d37523f59f414ae4fc8ab963839b6d3af
d3ltaaa <you@example.com>
2023-08-28 17:54:00 +0200

    important changes

--- setup/ssh_files/repl_ip_in_ssh_config.sh
+++ setup/ssh_files/repl_ip_in_ssh_config.sh
@@ -3,6 +3,8 @@
 # is to be installed on phone to send its own ip address to the computer in the network so that the 
 # computer can connect to the phone
 
+HOTSPOT_START=90 # specific to hotspot configuration (dynamic ip address 90 - *)
+
 if [[ ! -f "ip_address" ]]; then
 	touch ip_address
 fi
@@ -11,7 +13,9 @@ fi
 client_ip_address=$(ip a | grep -w swlan0: -A2 | awk 'NR==3 {print substr($2, 1, length($2) - 3)}')
 echo "$client_ip_address" > ip_address
 
-scp ip_address falk@192.168.2.31:/home/falk/.ssh/
+pc_ip_address=$(echo $client_ip_address | cut -d '.' -f 1-3)
+
+scp ip_address falk@$pc_ip_address.$HOTSPOT_START:/home/falk/.ssh/
 
 ssh falk@192.168.2.31 << 'EOF'
 line_number_phone=$(grep -n "PHONESPOT" /home/falk/.ssh/config | cut -d ':' -f 1)
