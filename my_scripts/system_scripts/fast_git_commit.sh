97cd3fe1853d40b912d31c6c8775637b4ce1ae2b
d3ltaaa <hil.falk@protonmail.com>
2023-12-23 19:52:22 +0100

    tests

--- my_scripts/system_scripts/fast_git_commit.sh
+++ my_scripts/system_scripts/fast_git_commit.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
-echo "Fast Git commit!"
-read -p "Give the commit a name!: " name
-sleep 1
-git add *
-git commit -m "$name"
-git push origin main
+if [[ "$(git status --porcelain)" != "" ]]; then
+	echo "Fast Git commit!"
+	read -p "Give the commit a name!: " name
+	sleep 1
+	firefox about:logins#%7B02b405cb-aa1e-404a-8d0f-9f45e2f2351a%7D &
+	git add *
+	git commit -m "$name"
+	git push origin main
+fi
