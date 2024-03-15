ab892eefc08d87fe6bf558f4bf8e48d8cb255d0f
d3ltaaa <hil.falk@protonmail.com>
2024-03-15 18:31:34 +0100

    wall

--- config_files/hypr/start.sh
+++ config_files/hypr/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 swww init &
-swww img ~/.config/swww/arch_nord_dark_bg.png &
+swww img ~/.config/wall/selected*
 
 waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
