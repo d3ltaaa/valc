0d933b47c371122480cde24d68e3ef5149f874e2
d3ltaaa <hil.falk@protonmail.com>
2024-03-29 23:37:35 +0100

    neorg fix

--- config_files/nvim/lua/plugins/neorg.lua
+++ config_files/nvim/lua/plugins/neorg.lua
@@ -1,35 +1,35 @@
 return {
 
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        -- tag = "*",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("neorg").setup({
-                load = {
-                    ["core.defaults"] = {}, -- Loads default behaviour
-                    ["core.concealer"] = {
-                        config = {
-                            folds = true,
-                            icon_preset = "diamond",
-                        },
-                    }, -- Adds pretty icons to your documents
-                    ["core.dirman"] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/Notes",
-                            },
-                            default_workspace = "notes",
-                        },
-                    },
-                    ["core.summary"] = {
-                        config = {
-                            strategy = "default",
-                        },
-                    },
-                },
-            })
-        end,
-    },
+  {
+    "nvim-neorg/neorg",
+    -- build = ":Neorg sync-parsers",
+    -- tag = "*",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      require("neorg").setup({
+        load = {
+          ["core.defaults"] = {},           -- Loads default behaviour
+          ["core.concealer"] = {
+            config = {
+              folds = true,
+              icon_preset = "diamond",
+            },
+          },                            -- Adds pretty icons to your documents
+          ["core.dirman"] = {           -- Manages Neorg workspaces
+            config = {
+              workspaces = {
+                notes = "~/Notes",
+              },
+              default_workspace = "notes",
+            },
+          },
+          ["core.summary"] = {
+            config = {
+              strategy = "default",
+            },
+          },
+        },
+      })
+    end,
+  },
 }
