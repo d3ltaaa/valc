785e2eb12486fa2aa563d401e7865609aca26e0e
d3ltaaa! <hil.falk@protonmail.com>
2024-10-22 21:59:37 +0000

    screenshot

--- my_scripts/app_names/screenshot_save.sh
+++ my_scripts/app_names/screenshot_save.sh
@@ -4,16 +4,16 @@ menu="rofi -dmenu -i"
 DIR="/home/falk/Pictures/Screenshots"
 # take screenshot and open
 grim -g "$(slurp)" "$DIR/temp.png" &&
-	feh --scale-down --auto-zoom "$DIR/temp.png" &&
-	selected=$(printf 'yes\nno' | $menu -p "Save?:")
+  feh --scale-down --auto-zoom "$DIR/temp.png" &&
+  selected=$(printf 'yes\nno' | $menu -p "Save?:")
 case $selected in
 "yes")
-	# give name
-	NAME="$($menu -p "Name:")"
-	FILE_NAME="$DIR/$NAME.png"
-	mv "$DIR/temp.png" "$FILE_NAME"
-	exit 0
-	;;
+  # give name
+  NAME="$($menu -p "Name:")"
+  FILE_NAME="$DIR/$NAME.png"
+  mv "$DIR/temp.png" "$FILE_NAME"
+  exit 0
+  ;;
 
 "*") ;;
 esac
