3561e4513db358c33b6473b88333eee92d85cf3f
d3ltaaa <hil.falk@protonmail.com>
2024-12-24 11:45:25 +0100

    hyprbars

--- config_files/hypr/start.sh
+++ config_files/hypr/start.sh
@@ -1,6 +1,8 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
 
- swww-daemon &
- swww img ~/.config/wall/selected*
+swww-daemon &
+swww img ~/.config/wall/selected*
 
- waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
+hyprpm reload -n
+waybar -c ~/.config/waybar/config -s ~/.config/waybar/style.css &
+
